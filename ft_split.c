6e5ce07c67f72d8969416ec5cff6aa9bcadd63ac
Red <redouane.azz76@gmail.com>
2025-10-24 23:15:48 +0200

    create a ft_split not finished

--- ft_split.c
+++ ft_split.c
@@ -6,41 +6,69 @@
 /*   By: red <red@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 18:53:22 by red               #+#    #+#             */
-/*   Updated: 2025/10/24 20:38:01 by red              ###   ########.fr       */
+/*   Updated: 2025/10/24 23:15:21 by red              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "libft.h"
 
+static size_t	ft_wordcount(const char *s, char c)
+{
+	size_t	i;
+	size_t	w;
+
+	i = 0;
+	w = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+			w++;
+		while (s[i] != c && s[i + 1])
+			i++;
+		i++;
+	}
+	return (w);
+}
+
+static char **fill_that(char *src, char **dst, char c)
+{
+  size_t i;
+  size_t x;
+  size_t len;
+
+  i = 0;
+  len = 0;
+  x =  0;
+  while ( x < ft_wordcount(src, c) && dst[i])
+  {
+    if (src[i] == c)
+    {
+      dst[x] = ft_substr(src, i-len, len);
+      while (src[i] == c)
+        i++;
+      x++;
+      len = 0;
+      i--;
+    }
+    i++;
+    len++;
+  }
+  return (dst);
+}
+
 char **ft_split(char const *s, char c)
 {
   char **ptr;
   char *str;
-  char c2[0];
-  size_t i;
-  size_t x;
+  char c2[] = "c";// a tej
 
-  if (!(ft_strchr(s, c)) )
-    return NULL;
-  c2[0] = c;
   str = ft_strtrim(s, c2);
-  i = 0;
-  while (str[i] == c)
-    i++;
-  i *= 2;
-  x = ft_strlen(s);
-  if (!(ptr = ft_calloc(i, sizeof(char *))))
+  if (!(ptr = ft_calloc(ft_wordcount(str, c), sizeof(char *))))
     return (NULL);
-  while (i > 0)
-  {
-    if (str[x] == c)
-      i--;
-    else
-      ptr[i]= ft_substr(str, x, ft_strlen(str) - x);
-    x--;
-  }
-  return (ptr);
+  
+  return (fill_that(str, ptr, c));
 }
+
 
 // cree une boucle pour allouer plusieur tableau >> determine le nom de str finaux >>
 // >> allouer le tab de tab en avec le nombre de tableau >> allouer les tableau en question avec le nombre de characté * (char *) >>
@@ -50,19 +78,19 @@ char **ft_split(char const *s, char c)
 
 // récupéré l'addresse de x et faie un substr pour importé automatique le str avec la bonne taille
 
-int main(int argc, char const *argv[])
-{
-  (void)argc;
-  char **ptr = ft_split(argv[1], argv[2][0]);
-  if (ptr)
-  {
-    while (*ptr)
-    {
-      printf("str: %s\n", *ptr);
-      ptr++;
-    }
+int main() {
+  char str[] = "Salutxcommentxtuxva";
+  char **words = ft_split(str, 'x');
+  if (words) {
+      printf("Mots séparés :\n");
+      printf("%s\n", words[0]);
+      printf("%s\n", words[1]);
+      printf("%s\n", words[2]);
+      printf("%s\n", words[3]);
+      free(words); // Libérer la mémoire allouée pour le tableau de mots
   } else {
-    printf("NULL\n");
+      printf("Erreur lors de l'allocation mémoire.\n");
   }
+
   return 0;
 }
