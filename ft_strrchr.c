c03a42f095769730f9b43a33091bcf5c0303012a
Red <redouane.azz76@gmail.com>
2025-10-24 14:51:54 +0200

    added somethings

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,18 +6,20 @@
 /*   By: red <red@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 19:22:44 by red               #+#    #+#             */
-/*   Updated: 2025/10/22 19:22:45 by red              ###   ########.fr       */
+/*   Updated: 2025/10/24 14:45:26 by red              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "libft.h"
 
-char	*strrchr(const char *string, int searchedChar)
+char	*ft_strrchr(const char *string, int searchedChar)
 {
 	size_t	i;
 
 	i = ft_strlen((char *)string);
 	while (i > 0 && string[i] != (char)searchedChar)
 		i--;
+	if (i == 0)
+		return(NULL);
 	return ((char *)&string[i]);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: red <red@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 17:36:47 by red               #+#    #+#             */
-/*   Updated: 2025/10/23 17:36:55 by red              ###   ########.fr       */
+/*   Updated: 2025/10/24 14:50:08 by red              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,5 +14,6 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-  
+  if(ft_strchr(set, s1[0]) != NULL && ft_strchr(set, s1[ft_strlen(s1) - 1]) != NULL)
+    return(ft_strdup())
 }
